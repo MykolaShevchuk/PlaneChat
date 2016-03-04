3718f3905a398ca09887994ceaaef6d1c2f195be
MykolaShevchuk <708029@gmail.com>
2016-03-04 15:02:37 +0200

    Change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(80);
 
 app.use('/app', express.static('app'));
 
